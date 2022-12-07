d9e830d6efc6bda669da6b38f4143803700bf175
heitork1 <heitorklein70@gmail.com>
2022-12-07 15:29:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function mudaCor(event){
+    let x = event.target  
+    let y = event.target.lastElementChild
+    
+    y.style.color = "#1CB0F6"
+    x.style.backgroundColor = "#DDF4FF"
+    x.style.border = "2px solid #84D8FF"
+    x.style.borderWidth = "2px"
+    x.style.borderColor = "#84D8FF"
+}
